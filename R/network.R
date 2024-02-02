7a3526cd20caae5f6eda06cb83d53484d9c9e5a6
Hayden Atchley <atchley.sha@protonmail.com>
2024-02-02 10:35:25 -0700

    m

--- R/network.R
+++ R/network.R
@@ -26,16 +26,27 @@ make_gmns_links <- function(links_file, nodes){
       to_node_id = B,
       length = DISTANCE,
       name = STREET,
-      link_id = LINKID,
+      link_id = paste(from_node_id, to_node_id, sep = "_"),
       zone_id = TAZID
     )
   
   nodes_geom <- nodes %>% 
     select(node_id, x_coord, y_coord)
   
-  links %>% 
+  links_geom %>% 
     left_join(nodes_geom, join_by(from_node_id == node_id)) %>% 
     rename_with(\(x) paste0(x, "_from"), x_coord:y_coord) %>% 
     left_join(nodes_geom, join_by(to_node_id == node_id)) %>% 
-    rename_with(\(x) paste0(x, "_to"), x_coord:y_coord)
+    rename_with(\(x) paste0(x, "_to"), x_coord:y_coord) %>% 
+    select(link_id, x_coord_from:y_coord_to) %>% 
+    rename_with(\(x) str_remove(x, "_coord")) %>% 
+    pivot_longer(
+      -link_id,
+      names_to = c("xy", "end"),
+      values_to = "coord",
+      names_sep = "_") %>%
+    pivot_wider(names_from = "xy", values_from = "coord") %>% 
+    mutate(end = factor(end, c("from", "to"))) %>% 
+    arrange(link_id, end) %>% 
+    sf_linestring("x", "y", linestring_id = "link_id")
 }
