5b88733357d6408c77eee7626fea4cbc103c1c82
Emily Blanchard <ekblnchrd@gmail.com>
2024-03-11 11:36:58 -0600

    Update with Dr. Mac's code

--- R/tc_functions.R
+++ R/tc_functions.R
@@ -10,21 +10,39 @@ estimate_tc <- function(nhts_per) {
 			WORKER == "01", 
 			WRKLOC_cat == 0
 		) %>%
-		dfidx(choice = "WKFMHM22", shape = "wide")
+		mutate(
+			children = case_when(
+				LIF_CYC_cat %in% c(1, 2) ~ "Children less than 5",
+				LIF_CYC_cat == 3 ~ "No children under 5",
+			),
+			female = ifelse(R_SEX == "02", TRUE, F),
+			children = factor(children, levels = c("No children under 5", 
+																						 "Children less than 5")),
+			tc_frequency = case_when(
+				WKFMHM22 == "01" ~ "Never", 
+				WKFMHM22 %in% c("02") ~ "One or two days", 
+				WKFMHM22 %in% c("03") ~ "Three or four days",
+				WKFMHM22 %in% c("04") ~ "Five or more days",
+				
+			)) |> 
+		select(HOUSEID, PERSONID, WTPERFIN, children, tc_frequency, female, HHVEHCNT_cat, HHFAMINC_cat) |> 
+		dfidx(choice = "tc_frequency", shape = "wide")
 	
 	null_model <- mlogit(
-		formula = WKFMHM22 ~ 1,
+		formula = tc_frequency ~ 1,
 		data = format_per,
 		weights = format_per$WTPERFIN
 	)
 	
 	models <- list(
-		# update(null_model, . ~ . -1 | LIF_CYC_cat + R_SEX),
-		update(null_model, . ~ . -1 | HHVEHCNT_cat + R_SEX)
-		# update(null_model, . ~ . -1 | LIF_CYC_cat + HHVEHCNT_cat + R_SEX),
-		# update(null_model, . ~ . -1 | LIF_CYC_cat + HHFAMINC_cat + R_SEX),
+		"basic" = update(null_model, . ~ . -1 | children + female),
+		"Model 1" = update(null_model, . ~ . -1 | children + HHVEHCNT_cat),
+		update(null_model, . ~ . -1 | children + HHVEHCNT_cat + female),
+		update(null_model, . ~ . -1 | children + female + HHFAMINC_cat),
+		update(null_model, . ~ . -1 | children*female + HHFAMINC_cat)
 		# update(null_model, . ~ . -1 | LIF_CYC_cat + HHVEHCNT_cat + HHFAMINC_cat + R_SEX)
 	)
-
+	
+	
 	return(models)
 }
